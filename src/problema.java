f6687e342475d3b1bad1b4652c87fafd99e5ae28
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-20 19:48:16 -0300

    Programa finalizado

--- src/problema.java
+++ src/problema.java
@@ -8,7 +8,9 @@ public class problema {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		double largura, altura;
+		double largura, altura, area, perimetro;
+		boolean flag = true;
+		int opcao;
 		
 		System.out.print("Digite a largura do retangulo: ");
 		largura = input.nextDouble();
@@ -31,6 +33,34 @@ public class problema {
 		}
 		
 		
+		while (flag) {
+			
+			System.out.println("\nMENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - Sair");
+			System.out.print("Digite uma opcao: ");
+			opcao = input.nextInt();
+			
+			if (opcao == 1) {
+				area = largura * altura;
+				System.out.printf("%nAREA = %.1f%n", area);
+			}
+			else if (opcao == 2) {
+				perimetro = (largura * 2) + (altura * 2);
+				System.out.printf("%nPERIMETRO = %.1f%n", perimetro);
+			}
+			else if (opcao == 3) {
+				flag = false;
+				break;
+			} 
+			else {
+				System.out.println("\nOPCAO INVALIDA");
+			}
+			
+		}
+		
+		System.out.println("FIM DO PROGRAMA!");
 		
 		input.close();
 
